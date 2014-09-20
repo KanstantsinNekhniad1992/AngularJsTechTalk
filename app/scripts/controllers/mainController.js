3811d7f625a88f57e7353e4d68db477f604e7c1f
KanstantsinNekhniad1992 <Kanstantsin_Nekhniadovich@epam.com>
2014-09-20 14:38:31 +0300

    add mainController

--- app/scripts/controllers/mainController.js
+++ app/scripts/controllers/mainController.js
@@ -1,6 +1,20 @@
 (function() {
+    'use strict';
+
     angular.module('controllers', [])
-        .controller('MainController', ['$rootScope', function($rootScope) {
-            $rootScope.world = "someTest";
+        .controller('MainController', ['$scope', '$http', function($scope, $http) {
+            $scope.postsModel = {};
+
+            $http.get('content/posts.json').success( function(data){
+                angular.forEach(data.posts, function(value,prop) {
+                    if(!angular.isDefined(value.image)) {
+                        data.posts[prop].image = 'images/noimage.gif';
+                    }
+                });
+                $scope.postsModel = data;
+            }).
+            error(function() {
+                $scope.postsModel.notFound = 'Posts not found!';
+            });
         }]);
 })();
